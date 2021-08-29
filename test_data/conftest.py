8c51c3161d2629e41cc3782a1b75ac6d8d24e10d
jojoconverteo <jonathan.ndamba@converteo.com>
2021-08-29 17:43:26 +0200

    mise a jour distrib

--- test_data/conftest.py
+++ test_data/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 import pandas as pd
+from pathlib import Path
 
 @pytest.fixture
 def data():
-    df_data_test = pd.read_csv("mock.csv")
+    p = Path('.')
+    names_csv = list(p.glob('test_data/*.csv'))
+    name_mock = [w for w in names_csv if "mock" in str(w)][0]
+    df_data_test = pd.read_csv(name_mock)
     return df_data_test
